556a937ca3435fd8f3c3d769e21ba772b8783c3e
saritha9087 <saritha2006r@gmail.com>
2026-06-20 02:42:47 +0530

    fix deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
@@ -9,15 +8,3 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
-=======
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./App.css";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
->>>>>>> 863432bb9fbdee09e24e51b176ae960c14f24e2c
--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
 import { defineConfig } from 'vite'
-<<<<<<< HEAD
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Mini-E-Commerce-Cart/'
 })
-=======
-
-export default defineConfig({
-  base: './Mini-E-Commerce-Cart/'
-})
->>>>>>> 863432bb9fbdee09e24e51b176ae960c14f24e2c
