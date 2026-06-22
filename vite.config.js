7d2379518df439b4efb65125572e4e6b213aaf4a
saritha9087 <saritha2006r@gmail.com>
2026-06-22 20:27:46 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/Mini-E-Commerce-Cart/'
+  plugins: [react()]
+  
 })
